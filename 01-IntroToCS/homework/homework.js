4a943642cb5aaa06ccb359a193d31294ee6f61cd
Martin Garcia Hervas <punighervas@gmail.com>
2023-08-01 10:15:35 -0300

    Finished

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,34 +1,44 @@
 'use strict';
 
 function BinarioADecimal(num) {
-   var number = num;
-   var numberarray = number.toString().split("").reverse();
-   var count = 0;
+   let number = num;
+   let numberarray = number.split("").reverse();
+   let count = 0;
 
-   for(var i=0; i < numberarray.length; i++){
+   for(let i=0; i < numberarray.length; i++){
       if(+numberarray[i] === 1){
-         count = count + (2 ** i)
+         count = count + (2 ** i)  // count = count + ------> count +=
       };
    }
    return count;
 }
 
+//Se puede usar el metodo Reduce() ----> medio falopa
+
 function DecimalABinario(num) {
-   var number = num;
-   var numberarray = [];
+   let number = num;
+   let numberarray = [];
    do {
       number = Math.floor(number)
       if(number % 2 === 0){
-         numberarray.push(0);
+         numberarray.unshift(0);
       } else {
-         numberarray.push(1);
+         numberarray.unshift(1);
       }
       number = number / 2;
    } while (number >= 1);
 
-   var binary = numberarray.reverse().join("");
+   let binary = numberarray.join("");
    return binary
 }
+
+// while(num => 1){
+//    let res = Math.trunc(num % 2);
+
+//    num = num / 2;
+//    numberarray.unshift(res);
+// }
+// return numberarray.join("")
 
 module.exports = {
    BinarioADecimal,
