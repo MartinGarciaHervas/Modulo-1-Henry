be6cd1a320c33aa5a43d0e885b1eb39d75e438a1
Martin Garcia Hervas <punighervas@gmail.com>
2023-08-09 13:45:12 -0300

    Finished

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,19 +6,18 @@ function factorear(numero) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-  let array = [];
+  let array = [1];
   let primo = 2;
    //dividir el nro por el nro primo mas chico posible //2 3 5 7 9 11 13 17 19 23
    while (numero > 1){
     while(numero % primo === 0){
         array.push(primo)
-        numero = numero / primo;
+        numero = numero / primo; // se puede escribir como numero /= primo
     } primo++
-   } array.unshift(1)
-   return array;
+   } return array;
 }
 
-function bubbleSort(newArray) {
+function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
@@ -27,15 +26,15 @@ function bubbleSort(newArray) {
 
  while(cambio){
   cambio = false;    //una vez que entra ya lo cambia directo a false
-  for(let i = 0; i+1 < newArray.length; i++){     //esto significa que es el anteulitmo elemento del array, ya si toma el ultimo no entraria a analizarse porq no tiene nada al lado
-    if(newArray[i] > newArray[i+1]){
-      let aux = newArray[i];
-      newArray[i] = newArray[i+1];
-      newArray[i+1] = aux;
+  for(let i = 0; i+1 < array.length; i++){     //esto significa que es el anteulitmo elemento del array, ya si toma el ultimo no entraria a analizarse porq no tiene nada al lado
+    if(array[i] > array[i+1]){
+      let aux = array[i];
+      array[i] = array[i+1];
+      array[i+1] = aux;
       cambio = true;
     }
   }
- } return newArray
+ } return array
 }
 
 function insertionSort(array) {
@@ -43,36 +42,42 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-  let newArray = array;
 
   for(let j = 1; j < array.length; j++){
-    for(let i = j-1; newArray[j] < newArray[i]; i-- && j--){
-      if(newArray[j] < newArray[i]){
-        let aux = newArray[j];
-        newArray[j] = newArray[i];
-        newArray[i] = aux;
+    for(let i = j-1; i >= 0 && array[j] < array[i]; i-- && j--){
+      if(array[j] < array[i]){
+        let aux = array[j];
+        array[j] = array[i];
+        array[i] = aux;
       } 
     }
-  } return newArray;
+  } return array;
 }
-console.log(insertionSort([5,4,3,2,1]));
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+
+  //      [7,  5,  2,  3 ,  1  ]
+  //       i
+  //                        j
+  // min = 7
   for(let i = 0; i < array.length; i++){
-    for(let j = 0; j < array.length; j++){
-      let actualMenor = array[i];
-      if(actualMenor < array[j]){
-        actualMenor = array[j];
-        array[j] = array[i];
-        array[i] = actualMenor
+    let actualMenor = i;
+    for(let j = i+1; j < array.length; j++){
+      if(array[actualMenor] > array[j]){
+        actualMenor = j;
       }
+    } if(array[i] > array[actualMenor]){
+      let aux = array[i];
+      array[i] = array[actualMenor];
+      array[actualMenor] = aux;
     }
   } return array;
 }
+console.log(selectionSort([7,  5,  2,  3 ,  1  ]));
 
 // No modificar nada debajo de esta línea
 // --------------------------------
