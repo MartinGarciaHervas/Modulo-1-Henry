d8965b10c8b228e714b2265fc7b75310aba95ab7
Martin Garcia Hervas <punighervas@gmail.com>
2023-08-09 16:47:09 -0300

    Finished

---
+++ 08-Algoritmos-II/homework/Prueba.js
@@ -0,0 +1,52 @@
+function mergeSort(array) {
+    // Implementar el método conocido como mergeSort para ordenar de menor a mayor
+    // el array recibido como parámetro
+    // Devolver el array ordenado resultante
+    // Tu código:
+  
+    //okey probemos ahora
+    //  [ 7 , 2 , 8 , 5 , 3 , 9 , 4  ]
+    // if( [1] ) => return array
+    // dividir([])
+    //  izq[]    |    der[]
+    /// merge(  mergeSort(izq[]), mergeSort(der[]) )
+    while(array[1]){
+        let izq = dividir(array)[0];
+        let der = dividir(array)[1];
+        return merge(mergeSort(izq), mergeSort(der))
+    } return array
+  }
+  
+  function dividir(array) {
+    // parte a la mitad
+    // devuelve izq y la derecha
+    let izq = [];
+    let der = [];
+    let mitad = Math.round(array.length / 2);
+
+    for(let i = 0; i < array.length ; i++){
+        if(i < mitad) izq.push(array[i]);
+        if(i >= mitad) der.push(array[i]);
+    } return [izq, der]
+  }
+  
+  function merge(izq, der) {
+    // compara ambos
+    // izq[]  < o >  der[]
+    //los concatena pero en orden
+    //[ ordenado ]
+    // if(izq > der) return der.concat(izq);
+    // if(izq < der) return izq.concat(der);
+    // let aux = izq.concat(der);
+    // return aux.sort((a,b) => a - b)
+    let res = [];
+    while (izq.length && der.length)
+        res.push(izq[0] < der[0] ? izq.shift() : der.shift());
+    return res.concat(izq.length ? izq : der);
+  }
+
+let arr = [18, 16, 2 ,3 ,4 ,5 , 6, 7, 198, 1 ];
+
+// // console.log(dividir(arr))
+// console.log(merge([7, 18],[16, 20]));
+console.log(mergeSort(arr));
--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,25 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  //  [ 7 , 2 , 8 , 5 , 3 , 9 , 4  ]
+  //
+  //  if([1]) => devuelvo el array
+  // let pivote = Math.random() --->>> chequen que el valor del indice este dentro del array
+  // recorrer el array i < pivote lo mando a la izq[] si i > pivote lo mando a la derecha[]
+  //             [1]          [1]
+  // quickSort(izq)   quickSort(der)
+  // return el array ordenado
+  while (array[1]) {
 
+    let pivot = Math.floor(Math.random() * array.length)
+    let izq = [];
+    let der = [];
+
+for(let i = 0; i < array.length; i++){
+    if(array[i] <= array[pivot] && i !== pivot) izq.push(array[i])
+    if(array[i] > array[pivot]) der.push(array[i]);   
+} return quickSort(izq).concat(array[pivot]).concat(quickSort(der))
+} return array;
 }
 
 function mergeSort(array) {
@@ -14,8 +32,31 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  while(array[1]){
+      let izq = dividir(array)[0];
+      let der = dividir(array)[1];
+      return merge(mergeSort(izq), mergeSort(der))
+  } return array
+}
 
-  //okey probemos ahora
+function dividir(array) {
+
+  let izq = [];
+  let der = [];
+  let mitad = Math.round(array.length / 2);
+
+  for(let i = 0; i < array.length ; i++){
+      if(i < mitad) izq.push(array[i]);
+      if(i >= mitad) der.push(array[i]);
+  } return [izq, der]
+}
+
+function merge(izq, der) {
+  
+  let res = [];
+  while (izq.length && der.length)
+      res.push(izq[0] < der[0] ? izq.shift() : der.shift());
+  return res.concat(izq.length ? izq : der);
 }
 
 // No modificar nada debajo de esta línea
